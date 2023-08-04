d94d7fc6eaf75c45e0034e06259c47aebc1f89df
Steve <ramiandrisoasteve@gmail.com>
2023-08-04 21:56:08 +0300

    health

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/health', function(req, res, next) {
+  console.log('GET health');
+  const getHealth = async (req, res) => {
+    return res.status(200).json({
+      message: "Server is alive, don't worry my friend 🙂",
+    });
+  };
+  res.status(200).json({
+    message: "Server is alive, don't worry my friend 🙂",
+  });
+  // res.render('index', { title: 'Express' });
 });
+
+const getHealth = async (req, res) => {
+  return res.status(200).json({
+    message: "Server is alive, don't worry my friend 🙂",
+  });
+};
 
 module.exports = router;
